bea8f6ad4d66fe3b6fc43230f1b1d4cba10ffab1
tervicke <ameywpatil@gmail.com>
2024-06-01 13:45:49 +0530

    added files

--- codeforces/381A.cpp
+++ codeforces/381A.cpp
@@ -3,23 +3,30 @@ using namespace std;
 int main(){
 	int n;
 	cin >> n;
+	int s,d = 0;
+	bool st = true;	
+	int bv = n-1;
+	int fv = 0;
 	int ar[n];
 	for(int i = 0 ; i < n ; i++){
 		cin >> ar[i];
 	}
-	sort(ar , ar + n , greater<int>());
-	long long s = 0 , d = 0;
-	for(int i = 0 ; i < n ; i++){
-		cout << ar[i] << " ";
-	}
-	cout << endl;
-	for(int i = 0 ; i < n ; i++){
-		if(i%2 == 0){
-			s+=ar[i];
+	while(n--){
+		int gv = 0;
+		if(ar[fv] > ar[bv]){
+			gv = ar[fv];
+			fv++;	
 		}else{
-			d+=ar[i];
+			gv = ar[bv];
+			bv--;
 		}
-	}
-	cout << s << " " <<d << endl;
-	return 0;
+		if(st){
+			s += gv; 
+		}
+		else{
+			d += gv;
+		}
+		st =!st;
+	}	
+	cout << s << " "<<d << endl;
 }
