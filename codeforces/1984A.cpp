9c4c9389581b713d7f1973ac7a79ca48fce4d785
tervicke <ameywpatil@gmail.com>
2024-06-13 19:51:29 +0530

    solve 1984A

---
+++ codeforces/1984A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	int t;
+	cin >> t;
+	while(t--){
+		int n;
+		cin >> n;
+		int a[n];
+		for(int i = 0 ; i < n ; i++){
+			cin >> a[i];
+		}
+		if(a[0] == a[n-1]){
+			cout << "NO" << endl;
+			continue;
+		}else{
+			cout << "YES" << endl;
+			string s(n,'R');
+			s[1]='B';
+			cout << s << endl;
+		}
+	}
+	return 0;
+}
