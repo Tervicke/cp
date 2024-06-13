d7b1115fb1f56322fc4b54ec7641cec4b9baed66
tervicke <ameywpatil@gmail.com>
2024-06-13 09:57:57 +0530

    update 1931C

--- codeforces/1931C.cpp
+++ codeforces/1931C.cpp
@@ -7,38 +7,34 @@ int main(){
 		int n;
 		cin >> n;
 		int ar[n];
-		for(int i=0;i<n;i++){
-			int x;
-			cin >> x;
-			ar[i] = x;
+		for(int i = 0 ; i < n ; i++){
+			cin >> ar[i];
 		}
-		int Lj=0;
-		int Li=0;
-		int m=ar[0];
-		for(int i=0;i<n-1;i++){
-			if(ar[i] != m){
-				Li=i-1;
+		int f=0;
+		int l=0;
+		for(int i = 0 ; i < n ; i++){
+			if(ar[0] == ar[i] ){
+				f++;
+			}else{
+				break;
 			}
-			if(ar[i] == m && ar[i+1] != m){
-					Li=i;
-					break;
-			}
-			Li=i;
 		}
-		for(int j=n-1;j>=1;j--){
-			if(ar[j] == m || ar[j-1] != m){
-					Lj=j;
-					break;
+		reverse(ar,ar+n);
+		for(int i = 0 ; i < n ; i++){ 
+			if(ar[0] == ar[i] ){
+				l++;
+			}else{
+				break;
 			}
-			Lj=j;
 		}
-		if(Li == n-1){
-			cout << "0" << endl;
-	 }
-		else{
-			Li++;
-			Lj--;
-			cout << Lj - Li + 1 << endl; 
+		if(ar[0] == ar[n-1]){
+			if(l==f && l==n){
+				cout << 0 << endl;
+			}else{
+				cout << n - (l + f) << endl;
+			}
+		}else{
+			cout << n - max(l,f) << endl;
 		}
 	}
 	return 0;
