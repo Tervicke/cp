76514a4f8f8b9a52a127f21e45e9092980daca7e
tervicke <ameywpatil@gmail.com>
2024-06-15 07:38:07 +0530

    solve 1904A

---
+++ codeforces/1904A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	int t;
+	cin >> t;
+	while(t--){
+		int a,b,x1,y1,x2,y2;
+		cin >> a >> b;
+		cin >> x1 >> y1;
+		cin >> x2 >> y2;
+		int ans = 0;
+		set<pair<int,int>> st1,st2;
+		for(int i = 0 ; i < 8 ; i++){
+			st1.insert({x1+,})
+		}
+	}
+	return 0;
+}
