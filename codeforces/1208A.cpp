fab76770d68a4ba3095a5b2d596603d3263b48ff
tervicke <ameywpatil@gmail.com>
2024-06-28 15:35:03 +0530

    solve 1208A

--- codeforces/1208A.cpp
+++ codeforces/1208A.cpp
@@ -1,25 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-long long f(long long n,long long a,long long b){
-	if(n==0){
-		return a;
-	}
-	if(n==1){
-		return b;
-	}
-	if(n==2){
-		return a^b;
-	}
-	return n ^ f(n-1,a,b);
-}
 int main(){
 	int t;
 	cin >> t;
 	while(t--){
 		long long a,b,n;
 		cin >> a >> b >> n;
-		cout << f(n,a,b) << endl;
+		if(n % 3 == 0){
+			cout << a << endl;
+		}
+		if(n % 3 == 1){
+			cout << b << endl;
+		}
+		if(n % 3 == 2){
+			cout << (a^b) << endl;
+		}
 	}
 	return 0;
 }
