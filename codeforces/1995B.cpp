c918ea4ff431e002641c3ad0b62d336fafd0d5fa
tervicke <ameywpatil@gmail.com>
2024-07-24 08:51:41 +0530

    solve 1995B

--- codeforces/1995B.cpp
+++ codeforces/1995B.cpp
@@ -1,37 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
+//ALL THE BEST
+void solve(){
+	long long n,m;
+	cin >> n >> m;
+	vector<int> a(n,0);
+	for(int i = 0 ; i < n ; i++){
+		cin >> a[i];
+	}
+	sort(a.begin(),a.end());
+	long long ans = 0;
+	long long sum = 0;
+	for(long long l = 0 , r = 0 ; l < n ;){
+		while(r<n && a[r]- a[l] <= 1 && sum + a[r] <= m){
+			sum += a[r];
+			r++;
+		}
+		ans = max(sum,ans);
+		sum -= a[l];
+		l++;
+	}
+	cout << ans << endl;
+}
 int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); 
+	cout.tie(0);
 	int t;
 	cin >> t;
 	while(t--){
-		int n,c,d;
-		cin >> n >> c >> d;
-		vector<int> b(n*n);
-		for(int i = 0 ; i < n*n ; i++){
-			cin >> b[i];
-		}
-		sort(b.begin(),b.end());
-		vector<int> req;
-		int a = b[0];
-		for(int i = 0 ; i < n ;i++){
-			for(int j = 0 ; j < n ; j++){
-				req.push_back(a + j*c);
-			}
-			a+=d;
-		}
-		sort(req.begin(),req.end());
-		bool flag = true;
-		for(int i =  0 ; i < n*n ; i++){
-			if(b[i] != req[i]){
-				flag = false;
-				break;
-			}
-		}
-		if(flag){
-			cout << "YES" << endl;
-		}else{
-			cout << "NO" << endl;
-		}
+		solve();
 	}
 	return 0;
 }
