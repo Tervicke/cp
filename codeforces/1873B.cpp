eeb906d4282806efd1483195c47d6a215e3830d7
tervicke <ameywpatil@gmail.com>
2024-08-03 12:04:57 +0530

    solve 1873B

--- codeforces/1873B.cpp
+++ codeforces/1873B.cpp
@@ -1,30 +1,45 @@
 #include <bits/stdc++.h>
+#include <bitset>
 using namespace std;
+#define ll long long
+//ALL THE BEST
+void solve(){
+	int n;
+	cin >> n;
+	int p= 1;
+	int mx = INT_MIN;
+	vector<int> v(n);
+	int zero = 0;
+	for(int i = 0 ; i < n ; i++){
+		cin >> v[i];
+		if( v[i]!=0 ){
+			p*=v[i];
+		}else{
+			zero++;
+		}
+	}
+	if(zero >= 2){
+		cout << 0 << endl;
+		return;
+	}else if(zero == 0){
+		for(int i = 0 ; i < n ; i++){
+			mx = max(mx,(p/v[i])*(v[i]+1 ));
+		}
+		cout << mx << endl;
+		return;
+	}else{
+		cout << p << endl;
+	}
+}
 int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); 
+	cout.tie(0);
 	int t;
 	cin >> t;
 	while(t--){
-		int n;
-		cin >> n;
-		int ar[n];
-		int p = 1;
-		int m = 10e5;
-		int t = 0;
-		for(int i = 0 ; i < n ; i++){
-			int x;
-			cin >> x;
-			m = min(m,x);
-			if(x==0){
-				t=1;
-			}else{
-				p*=x;
-			}
-		}
-		if(t==1){
-			cout << p << endl;
-		}else{
-			cout << (p/m)*(m+1) << endl;
-		}
+		solve();
 	}
 	return 0;
 }
+
