3f2b63ad1f6c8280b9ca04b2cc53bdeff20c7879
tervicke <ameywpatil@gmail.com>
2024-12-01 10:29:42 +0530

    solve 2039A

---
+++ codeforces/2039A.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include <bit>
+using namespace std;
+#define ll long long
+//ALL THE BEST
+const long long MOD = 10e9 + 7;
+void solve() {
+	int n;
+	cin >> n;
+	int no = 1;
+	for(int i = 0 ; i < n ; i++){
+		cout << no << " ";
+		no+=2;
+	}
+	cout << "\n";
+}
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	bool multiple_test_cases = true;
+	int t = 1;
+	if(multiple_test_cases){
+		cin >> t;
+	}
+	while(t--){
+		solve();
+	}
+	return 0;
+}
